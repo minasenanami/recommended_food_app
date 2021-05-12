d16e91d34f35523f47cd793254b5dc05cc8a585b
僚 <>
2021-05-12 22:10:11 +0900

    food の新規投稿ページの実装

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,15 +1,35 @@
 class FoodsController < ApplicationController
-  def index; end
+  def index
+    @foods = Food.order(created_at: :desc)
+  end
 
-  def new; end
+  def new
+    @food = Food.new
+  end
 
-  def create; end
+  def create
+    @food = current_user.foods.create!(food_params)
+  end
 
-  def show; end
+  def show
+    @food
+  end
 
-  def edit; end
+  def edit
+    @food
+  end
 
-  def update; end
+  def update
+    @food
+  end
 
-  def destroy; end
+  def destroy
+    @food
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :comment)
+  end
 end
