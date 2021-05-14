a2bbbe97f7fcacca740a56e719446efcea0a8169
僚 <>
2021-05-14 15:08:29 +0900

    各モデルのバリテーションを設定

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,4 +3,6 @@ class Food < ApplicationRecord
 
   validates :name, presence: true
   belongs_to :user
+  has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user
 end
